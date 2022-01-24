f261ff746dbce399f9a05ae93817b7d55fedb33c
luke choi <primera_jh@hotmail.com>
2022-01-24 13:51:37 +0900

    sign up page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:eighth_flutter_app/login_page.dart';
+import 'package:eighth_flutter_app/signup_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage()
+      home: SignUpPage()
 
     );
   }
